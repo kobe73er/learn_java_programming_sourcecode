c0d890b643819f997de3b31e22dd5d545f8f6705
andrew.deng <andrew.deng@coupang.com>
2018-03-02 15:55:55 +0800

    init

---
+++ src/HelloWorldMultiMessage.java
@@ -0,0 +1,7 @@
+public class HelloWorldMultiMessage {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Programming with!");
+        System.out.println("Java is Fun!");
+    }
+}
